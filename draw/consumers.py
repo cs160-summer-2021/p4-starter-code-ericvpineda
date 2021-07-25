4c66c31270381703679a1f27c151fee5a1e7793e
Eric Pineda <ericvpineda@berkeley.edu>
2021-07-25 13:38:25 -0700

    completed project 4.8

--- draw/consumers.py
+++ draw/consumers.py
@@ -1,6 +1,5 @@
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
-import json
 
 class DrawConsumer(WebsocketConsumer):
     def connect(self):
--- draw/models.py
+++ draw/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.db.models.fields import CharField
 
 # Create your models here.
+class User(models.Model):
+    uid = models.CharField(max_length=100)
+    strokeColor = models.CharField(max_length=8)
